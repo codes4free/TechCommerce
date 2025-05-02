5aac6459d2ba6c2d6563bf41d726ae4675fb9586
d6lab <ffmtelecom@gmail.com>
2025-05-02 16:33:55 -0300

    Add settings.py and urls.py - Secretkey

---
+++ settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS += [
+    "rest_framework",
+    "rest_framework_simplejwt",
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
